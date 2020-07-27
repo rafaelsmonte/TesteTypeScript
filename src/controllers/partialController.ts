fd130fd82a0610d0cf9157135a8a3346edbd6bb4
Rafael Santos Monte <rsmonte@hotmail.com>
2020-07-26 21:32:38 -0300

    arrumado partial controller

--- src/controllers/partialController.ts
+++ src/controllers/partialController.ts
@@ -1,35 +1,32 @@
 import mongoose from "mongoose";
-import { Router, Request, Response } from 'express';
+import { Request, Response } from 'express';
 import partial from '../models/partial'
-
-
 export default {
   todasPropostas(req:Request, res:Response){
     res.status(201).send("ok partial");
   },
   gravaProposta(req:Request, res:Response){
     const { token } = req.body;
+
     if(token)
       res.status(201).send("update");
     else
     {
       const novaProposta = new partial(req.body);
+      
+      var dataSet =novaProposta.toJSON();
+      dataSet.sucess = true ;
+      dataSet.token = dataSet._id;
+      dataSet._id=undefined;
       novaProposta.save()
-      .then(x=> {
-        res.status(201).send({
-          novaProposta,
-          "success": true,
-        });
-      })
-      .catch(e => {
+      .then(x=> { res.status(201).send(dataSet);
+      }).catch(e=> {
         return res.status(400).send({
             Mensagem: "Erro ao cadastrar a proposta",
+            "success": false,
             Data: e
-        });
-
+        })
+      });
     }
-
-
   }
-
 }
